a7655a19df8eb7e11fa3d85cb79bab50ea40028a
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-03 23:18:56 +0200

    Initial application as generated by rebar

---
+++ src/versionserver_app.erl
@@ -0,0 +1,16 @@
+-module(versionserver_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    versionserver_sup:start_link().
+
+stop(_State) ->
+    ok.
