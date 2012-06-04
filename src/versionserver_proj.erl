b530ef3e126b685164e270d612a276e473dce75b
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-04 14:22:44 +0200

    Skeleton of the project worker

--- src/versionserver_proj.erl
+++ src/versionserver_proj.erl
@@ -1,14 +1,23 @@
--module(versionserver_repo).
+-module(versionserver_proj).
 
 -behaviour(gen_server).
 
+%% API functions
+-export([start_link/1]).
+
 %% Server callbacks
--export([]).
+-export([init/1]).
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
+start_link(Project) when is_atom(Project) ->
+	gen_server:start_link(?MODULE, Project, []).
+
 %% ===================================================================
 %% Server callbacks
 %% ===================================================================
+
+init(Project) ->
+	.
