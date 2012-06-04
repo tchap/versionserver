432cf564f9ff072ecac487ef575967d97af17eb6
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-04 22:38:11 +0200

    On the way to versionserver_proj

--- src/versionserver_proj.erl
+++ src/versionserver_proj.erl
@@ -6,9 +6,10 @@
 -export([start_link/1]).
 
 %% Server callbacks
--export([init/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
 
--record(state, {versions})
+-record(state, {table})
 
 %% ===================================================================
 %% API functions
@@ -22,20 +23,54 @@ start_link(Project) when is_atom(Project) ->
 %% ===================================================================
 
 init(Project) ->
-	.
+	{ok, #state{table=Project}}.
 
-handle_call({get_build_number, 
-	     Proj, Version={Maj, Min, Rel}, Id}, _From, State) ->
-	.
+handle_call(_Request, _From, State) ->
+	{reply, ok, State}.
+
+handle_cast({get_build_number, Version, Id}, _From, State) ->
+	case get_build_number(Proj, Version) of
+		Build when is_integer(Build) ->
+			gen_server:reply(Id, Build),
+			ok = set_build_number(State#state.table,
+					      Version, Build + 1),
+			{noreply, State};
+		{error, Reason} ->
+			gen_server:reply(Id, {error, Reason}),
+			{stop, Reason, State};
+		_ ->
+			Reason = unknown_error,
+			gen_server:reply(Id, {error, Reason}),
+			{stop, Reason, State}
+	end.
 
 handle_cast({delete_project, Proj}, State) ->
-	.
+	{noreply, State}.
 
 handle_info(_Info, State) ->
 	{noreply, State}.
 
-terminate(Reason, State) ->
+terminate(_Reason, _State) ->
 	ok.
 
-code_change(OldVsn, State, Extra) ->
+code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
+
+%% ===================================================================
+%% Private functions
+%% ===================================================================
+
+get_build_number(Proj, {Major, Minor, Release}) ->
+	Version = list_to_atom(lists:concat([Maj, '.', Min, '.', Rel])),
+	Pattern = {Proj, Version, '$1'},
+	Fun = fun() -> mnesia:dirty_match_object(Pattern) end,
+	try mnesia:sync_dirty(Fun, []) of
+		{Proj, Version, Build} when is_integer(Build) ->
+			Build;
+		_ -> {error, unknown_error}
+	catch exit:Exit ->
+		{error, Exit}
+	end.
+
+set_build_number(Proj, {Major, Minor, Release}, Build) ->
+	ok.
