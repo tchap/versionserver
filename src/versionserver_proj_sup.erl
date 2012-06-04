1c480a4bfb4237faee4c13c6003168d699ce90b6
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-04 14:07:47 +0200

    Initial version of the projects supervisor

--- src/versionserver_proj_sup.erl
+++ src/versionserver_proj_sup.erl
@@ -1,28 +1,27 @@
-
--module(versionserver_repo_sup).
+-module(versionserver_proj_sup).
 
 -behaviour(supervisor).
 
-%% API
+%% API functions
 -export([start_link/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
-
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+	supervisor:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
-	{ok, { {simple_one_for_one, 5, 10}, []} }.
-
+	{ok, { {simple_one_for_one, 5, 10},
+	       [{project,
+		 {versionserver_proj, start_link, []},
+		 permanent, 5000, worker, [versionserver_proj]}
+	       ]} }.
