eb0dbd0a7d93c26ee1a23763ec2c18e8e067c842
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-05 18:29:30 +0200

    On the way to the next generation

--- src/versionserver.erl
+++ src/versionserver.erl
@@ -1,62 +1,69 @@
--module(versionserver_serv).
+-module(versionserver).
 
 -behaviour(gen_server).
 
--define(PROJ_SUP, version_proj_sup).
+-define(PROJ_MODULE, versionserver_proj).
 
 %% API functions
--export([start/1, start_link/1, get_build_number/3, delete_project/2, stop/1]).
+-export([start/0, start_link/0, stop/0,
+	 get_build_number/3, set_build_number/3, delete_project/2]).
 
 %% Server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
 
-%% Server state record
--record(state, {projects=dict:new()}).
-
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
-start(Name) ->
-	gen_server:start({local, Name}, ?MODULE, [], []).
+start() ->
+	gen_server:start({local, ?MODULE}, ?MODULE, [], []).
 
-start_link(Name) ->
-	gen_server:start_link({local, Name}, ?MODULE, [], []).
+start_link() ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-get_build_number(Name, Proj, Version={Maj, Min, Rel})
-    when is_atom(Proj), is_integer(Maj), is_integer(Min), is_integer(Rel) ->
-	gen_server:call(Name, {build_number, Proj, Version}).
+get_build_number(Proj, Version={Maj, Min, Rel})
+    when is_atom(Proj), lists:all(fun erlang:is_integer/1, 
+	    			  [Maj, Min, Rel]) ->
+	gen_server:call(?MODULE, {get_build_number, Proj, Version}).
 
-delete_project(Name, Proj) when is_atom(Proj) ->
-	gen_server:cast(Name, {delete_project, Proj}).
+set_build_number(Proj, Version={Maj, Min, Rel}, Build) ->
+    when is_atom(Proj), lists:all(fun erlang:is_integer/1, 
+	    			  [Maj, Min, Rel, Build]) ->
+	gen_server:cast(?MODULE, {set_build_number, Proj, Version, Build}).
 
-stop(Name) ->
-	gen_server:call(Name, stop).
+delete_project(Proj) when is_atom(Proj) ->
+	gen_server:cast(?MODULE, {delete_project, Proj}).
+
+stop() ->
+	gen_server:call(?MODULE, stop).
 
 %% ===================================================================
 %% Server callbacks
 %% ===================================================================
 
 init([]) ->
-	State = #state{},
-	{ok, State}.
+	process_flag(trap_exit, true),
+	ets:new(?MODULE, [set, named_table])},
+	{ok, none}.
 
-handle_call({build_number, Proj, Version}, From, State) ->
-	Projects = State#state.projects,
-	case dict:find(Proj, Projects) of
-		{ok, Id} ->
-			gen_server:cast(Id, {build_number, Proj, Version, From}),
-			{noreply, State};
-		error ->
-			Id = supervisor:start_child(?PROJ_SUP, Proj),
-			UpdatedProjects = dict:store(Proj, Id, Projects),
-			gen_server:cast(Id, {build_number, Proj, Version, From}),
-			{noreply, State#state{projects=UpdatedProjects}}
-	end;
+handle_call({get_build_number, Proj, Version}, From, State) ->
+	Request = {get_build_number, Version, From},
+	ProjPid = case ets:lookup(?MODULE, Proj) of
+		[{Proj, Pid}] ->
+			Pid;
+		[] ->
+			Pid = gen_server:start_link(?PROJ_MODULE, [Proj], []),
+			ets:insert(?MODULE, {Proj, Pid}),
+			Pid
+	end,
+	gen_server:cast(Pid, Request),
+	{noreply, State};
 handle_call(stop, _From, State) ->
 	{stop, normal, ok, State}.
 
+handle_cast(Msg={set_build_number, _Version, _Build}, State) ->
+	{noreply, State};
 handle_cast(Msg={delete_project, Proj}, State) ->
 	Projects = State#state.projects,
 	case dict:find(Proj, Projects) of
@@ -68,6 +75,8 @@ handle_cast(Msg={delete_project, Proj}, State) ->
 			{noreply, State}
 	end.
 
+handle_info({'EXIT', Pid, _Reason}, State) ->
+	{noreply, State};
 handle_info(_Info, State) ->
 	{noreply, State}.
 
@@ -76,3 +85,7 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
+
+%% ===================================================================
+%% Private functions
+%% ===================================================================
